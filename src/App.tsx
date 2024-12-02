3d57b887a43c399a90340e9cde2453bb3299cc2b
Mohamed Lasheen <mohamedlasheen637@gmail.com>
2024-12-02 22:32:42 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-purple-500 font-semibold italic text-center m-3">React Todo Project</h1>
+
+    </>
+  )
+}
+
+export default App
